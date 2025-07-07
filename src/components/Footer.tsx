72a9db0b8ba2f7986f8a2d22ac45e745597c026b
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2025-07-07 15:16:58 -0400

    footer style update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="py-8 text-center text-sm text-gray-800 ">
+      <footer className="py-8 text-center text-sm bg-[#0c1220] text-white ">
         © {new Date().getFullYear()} Durai — Built with React & Tailwind.
       </footer>
     );
